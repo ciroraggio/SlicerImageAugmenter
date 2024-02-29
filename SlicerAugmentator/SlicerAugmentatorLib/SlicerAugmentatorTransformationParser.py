72519e06fbc8577b387f5cd7dfd6bbf1790f7491
Ciro Raggio <ciroraggio25@gmail.com>
2024-02-29 15:51:14 +0100

    Added intensity transforms Refactored transforms

--- SlicerAugmentator/SlicerAugmentatorLib/SlicerAugmentatorTransformationParser.py
+++ SlicerAugmentator/SlicerAugmentatorLib/SlicerAugmentatorTransformationParser.py
@@ -1,9 +1,6 @@
 import slicer
-try:
-  from monai.transforms import Rotate, Flip, Resize, RandRotate, RandScaleIntensity, RandAdjustContrast
-except ModuleNotFoundError:
-  slicer.util.pip_install("monai[itk]")
-  from monai.transforms import Rotate, Flip, Resize, RandRotate, RandScaleIntensity, RandAdjustContrast
+from SlicerAugmentatorLib.SlicerAugmentatorIntensity import mapIntensityTransformations
+from SlicerAugmentatorLib.SlicerAugmentatorSpatial import mapSpatialTransformations
 
 
 try:
@@ -18,56 +15,9 @@ def getSerializedObject(dictionary: dict) -> Munch :
 def mapTransformations(ui) -> list:
     mappedTransformations = []
     transformations = getSerializedObject(getTransformations(ui))
-    
-    if(transformations.rotate.enabled):
-        mappedTransformations.append(Rotate(angle=float(transformations.rotate.angle), 
-                                            mode=transformations.rotate.interpolationMode
-                                            ))
-    if(transformations.randRotate.enabled):
-      rangeX, rangeY, rangeZ = [0,0], [0,0], [0,0]
-      try:
-        if(transformations.randRotate.rangeFromX != "" and transformations.randRotate.rangeToX != "" ):
-            rangeX = [float(transformations.randRotate.rangeFromX), float(transformations.randRotate.rangeToX)]
-        if(transformations.randRotate.rangeFromY != "" and transformations.randRotate.rangeToY != "" ):
-            rangeY = [float(transformations.randRotate.rangeFromY), float(transformations.randRotate.rangeToY)]
-        if(transformations.randRotate.rangeFromZ != "" and transformations.randRotate.rangeToZ != "" ):
-            rangeZ = [float(transformations.randRotate.rangeFromZ), float(transformations.randRotate.rangeToZ)]
-                                                
-        mappedTransformations.append(RandRotate(prob=1,
-                                range_x=rangeX, 
-                                range_y=rangeY, 
-                                range_z=rangeZ, 
-                                padding_mode=transformations.randRotate.paddingMode, 
-                                mode=transformations.randRotate.interpolationMode, 
-                                keep_size=True, 
-                                align_corners=transformations.randRotate.alignCorners))
-      except Exception as e:
-        raise ValueError(e)      
-      
-                                     
-    if(transformations.resize.enabled):
-        mappedTransformations.append(Resize(spatial_size=(transformations.resize.spatialSize),
-                                            mode=transformations.resize.interpolationMode 
-                                            ))
-    if(transformations.flip.enabled):
-        mappedTransformations.append(Flip(spatial_axis=int(transformations.flip.axes)))
-        
-    if(transformations.randomScaleIntensity.enabled):
-        if(transformations.randomScaleIntensity.factor == ""):
-          raise ValueError("The 'Random Scale Intensity' transformation is enabled but a factor is not specified")      
-        
-        mappedTransformations.append(RandScaleIntensity(prob=1, factors=float(transformations.randomScaleIntensity.factor)))    
-        
-    if(transformations.randomAdjustContrast.enabled):
-        if(transformations.randomAdjustContrast.gammaFrom == "" or transformations.randomAdjustContrast.gammaTo == ""):
-          raise ValueError("The 'Random Adjust Contrast' transformation is enabled but gamma values is not specified")      
-        
-        mappedTransformations.append(RandAdjustContrast(prob=1, 
-                                                        gamma=[float(transformations.randomAdjustContrast.gammaFrom), float(transformations.randomAdjustContrast.gammaTo)],
-                                                        invert_image=transformations.randomAdjustContrast.invertImage,
-                                                        retain_stats=True
-                                                        )
-                                     )    
+
+    mappedTransformations.extend(mapSpatialTransformations(mappedTransformations=mappedTransformations,transformations=transformations))  
+    mappedTransformations.extend(mapIntensityTransformations(mappedTransformations=mappedTransformations,transformations=transformations))  
         
     return mappedTransformations
 
@@ -112,6 +62,20 @@ def getTransformations(ui) -> dict:
                     "gammaFrom": ui.randomAdjustContrastGammaFrom.text,
                     "gammaTo": ui.randomAdjustContrastGammaTo.text,
                     "invertImage":ui.randomAdjustContrastInvertImage.isChecked(),
+                },
+                "randomGaussianNoise": {
+                    "enabled": ui.randomGaussianNoiseEnabled.isChecked(),
+                    "mean": ui.randomGaussianNoiseMean.text,
+                    "std": ui.randomGaussianNoiseStd.text,
+                },
+                "shiftIntensity": {
+                    "enabled": ui.shiftIntensityEnabled.isChecked(),
+                    "offset": ui.shiftIntensityOffset.text,
+                },
+                "randomShiftIntensity": {
+                    "enabled": ui.randomShiftIntensityEnabled.isChecked(),
+                    "offsetFrom": ui.randomShiftIntensityFrom.text,
+                    "offsetTo": ui.randomShiftIntensityTo.text,
                 }
             }
       
